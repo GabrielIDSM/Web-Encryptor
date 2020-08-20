54590fb1c85b9aea72bef64b7f2a9d895d84bee4
GabrielInacio <gabrielinacio@id.uff.br>
2020-08-20 08:07:20 -0300

    Machines and About components created

--- src/Machines.js
+++ src/Machines.js
@@ -3,7 +3,7 @@ import './App.css';
 
 function Machines() {
     return(
-        <div>
+        <div className="Machines">
 
         </div>
     );
--- src/Navbar.js
+++ src/Navbar.js
@@ -1,14 +1,21 @@
 import React from 'react';
 import './App.css';
+import { Link } from 'react-router-dom';
 
 function NavBar() {
   return (
     <nav className="Navbar">
       <p className="Title"><b>Encryptor</b></p>
       <ul className="Navlinks">
-        <li>Home</li>
-        <li>Machines</li>
-        <li>About</li>
+        <Link className="links" to="/">
+          <li>Home</li>
+        </Link>
+        <Link className="links" to="/Machines">
+          <li>Machines</li>
+        </Link>
+        <Link className="links" to="/About">
+          <li>About</li>
+        </Link>
       </ul>
     </nav>
   );
