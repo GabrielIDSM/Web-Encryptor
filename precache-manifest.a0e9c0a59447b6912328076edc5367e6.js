edb348562b4be95979a8e0060c4d21d5737417c8
GabrielInacio <gabrielinacio@id.uff.br>
2020-09-14 08:33:03 -0300

    Updates

---
+++ precache-manifest.a0e9c0a59447b6912328076edc5367e6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ee9fab59b59e30bfb4a2b439fee34a5e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "517c26d923a3690b2d65",
+    "url": "/static/css/main.980dc213.chunk.css"
+  },
+  {
+    "revision": "fde51af0ccac76405d8f",
+    "url": "/static/js/2.76a1e36c.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.76a1e36c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "517c26d923a3690b2d65",
+    "url": "/static/js/main.d3cd6676.chunk.js"
+  },
+  {
+    "revision": "67def4d736a2cedf1376",
+    "url": "/static/js/runtime-main.a0df7873.js"
+  }
+]);
