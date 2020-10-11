5d286d6db15048b9a08012fa642b35294b87eda4
GabrielInacio <gabrielinacio@id.uff.br>
2020-10-11 10:40:30 -0300

    Updates

--- precache-manifest.3ffa12deec8ad2f3efa5beb251df964e.js
+++ precache-manifest.3ffa12deec8ad2f3efa5beb251df964e.js
@@ -1,11 +1,11 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "9d1f2fbfe34c18a7fe718092f807e44e",
+    "revision": "a2f6354881bae314483b848aeb22f85c",
     "url": "/Web-Encryptor/index.html"
   },
   {
-    "revision": "aab2646c0603be5fd779",
-    "url": "/Web-Encryptor/static/css/main.980dc213.chunk.css"
+    "revision": "7f68e0539c82636ed9bd",
+    "url": "/Web-Encryptor/static/css/main.7a4c9a9f.chunk.css"
   },
   {
     "revision": "57048b350f2fc37c5f4b",
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/Web-Encryptor/static/js/2.3bc16d62.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "aab2646c0603be5fd779",
-    "url": "/Web-Encryptor/static/js/main.9c8567f5.chunk.js"
+    "revision": "7f68e0539c82636ed9bd",
+    "url": "/Web-Encryptor/static/js/main.cdfc94f6.chunk.js"
   },
   {
     "revision": "753f72dfcb6587e99865",
