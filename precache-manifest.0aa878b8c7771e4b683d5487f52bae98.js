0c5b8beeceb6859a8112e5adf4beee86c8d80ad6
gabriel <gabrielinacio@id.uff.br>
2021-02-21 08:19:02 -0300

    Updates

--- precache-manifest.0aa878b8c7771e4b683d5487f52bae98.js
+++ precache-manifest.0aa878b8c7771e4b683d5487f52bae98.js
@@ -1,11 +1,11 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "6258e20cb54c24ee38d4a62d50fed7f6",
+    "revision": "f77bb85051fd0dc47327310938933f2c",
     "url": "/Web-Encryptor/index.html"
   },
   {
-    "revision": "575606f0d93f94145cd6",
-    "url": "/Web-Encryptor/static/css/main.6bb63c23.chunk.css"
+    "revision": "adad2b9868539b709480",
+    "url": "/Web-Encryptor/static/css/main.ffdce8f0.chunk.css"
   },
   {
     "revision": "ba40e779898a2e76b729",
@@ -16,7 +16,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/Web-Encryptor/static/js/2.ae9de300.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "575606f0d93f94145cd6",
+    "revision": "adad2b9868539b709480",
     "url": "/Web-Encryptor/static/js/main.9940f77c.chunk.js"
   },
   {
